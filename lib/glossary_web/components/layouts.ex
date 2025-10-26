4e2eb19379e650e5568664aee5544181521b667f
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-26 18:08:43 -0500

    Add navbar layout component

--- lib/glossary_web/components/layouts.ex
+++ lib/glossary_web/components/layouts.ex
@@ -12,6 +12,32 @@ defmodule GlossaryWeb.Layouts do
   embed_templates "layouts/*"
 
   @doc """
+  Glossary navbar.
+
+  Drawn at the top of pages using <Layouts.app flash={@flash}>.
+  """
+
+  def navbar(assigns) do
+    ~H"""
+    <nav class="navbar px-5">
+      <div class="flex-1">
+        <a href="/" class="flex-1 flex w-fit items-center gap-4">
+          <span class="text-3xl">🧠</span>
+          <span class="text-xl text-base-content font-bold">Glossary</span>
+        </a>
+      </div>
+      <div class="flex-none">
+        <ul class="flex flex-column px-1 space-x-4 items-center">
+          <li>
+            <.theme_toggle />
+          </li>
+        </ul>
+      </div>
+    </nav>
+    """
+  end
+
+  @doc """
   Renders your app layout.
 
   This function is typically invoked from every template,
@@ -35,32 +61,7 @@ defmodule GlossaryWeb.Layouts do
 
   def app(assigns) do
     ~H"""
-    <header class="navbar px-4 sm:px-6 lg:px-8">
-      <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
-        </a>
-      </div>
-      <div class="flex-none">
-        <ul class="flex flex-column px-1 space-x-4 items-center">
-          <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
-          </li>
-          <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
-          </li>
-          <li>
-            <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </header>
+    <.navbar />
 
     <main class="px-4 py-20 sm:px-6 lg:px-8">
       <div class="mx-auto max-w-2xl space-y-4">
