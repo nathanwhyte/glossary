6ca55bc7f9d9c86a8a9fe4707a7dcf330c7f7332
Nathan Whyte <nathanwhyte35@gmail.com>
2025-10-27 00:15:19 -0500

    Add tests for quick start content skeletons

--- test/glossary_web/live/home_live_test.exs
+++ test/glossary_web/live/home_live_test.exs
@@ -22,4 +22,25 @@ defmodule GlossaryWeb.HomeLiveTest do
     assert has_element?(view, "code", "#subject")
     assert has_element?(view, "code", "!")
   end
+
+  test "renders quick start content", %{conn: conn} do
+    {:ok, view, _html} = live(conn, "/")
+
+    # with keybinds
+    assert has_element?(view, "span", "New Entry")
+    assert has_element?(view, "kbd", "⌘")
+    assert has_element?(view, "kbd", "shift")
+    assert has_element?(view, "kbd", "O")
+
+    assert has_element?(view, "span", "View Last Entry")
+    assert has_element?(view, "kbd", "⌘")
+    assert has_element?(view, "kbd", "shift")
+    assert has_element?(view, "kbd", "S")
+
+    # just links
+    assert has_element?(view, "span", "Get a Refresher")
+    assert has_element?(view, "span", "View All Tags")
+    assert has_element?(view, "span", "View All Subjects")
+    assert has_element?(view, "span", "View AI-Assisted Summaries")
+  end
 end
