dc7313df2caf6e114694b8b9dadf0d7f68617963
Nathan Whyte <nathanwhyte35@gmail.com>
2025-12-18 18:30:52 -0600

    Use port 5433 for Postgres, not usually taken already

--- config/test.exs
+++ config/test.exs
@@ -13,6 +13,7 @@ config :glossary, Glossary.Repo,
   password: "postgres",
   hostname: "localhost",
   database: "glossary_test#{System.get_env("MIX_TEST_PARTITION")}",
+  port: 5433,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
