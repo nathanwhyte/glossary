da5553dc43b9b26b7ddcd4614cf4558bc6da1bbb
Nathan Whyte <nathanwhyte35@gmail.com>
2026-01-31 12:44:23 -0600

    Add Entry seeds for db

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,37 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+
+alias Glossary.Repo
+alias Glossary.Entries
+alias Glossary.Entries.Entry
+
+entries = [
+  %{
+    title: "Lorem Ipsum One",
+    subtitle: "Single paragraph",
+    body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  %{
+    title: "Lorem Ipsum Two",
+    subtitle: "Two paragraphs",
+    body:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\nSed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem."
+  }
+]
+
+{inserted, skipped} =
+  Enum.reduce(entries, {0, 0}, fn attrs, {inserted_count, skipped_count} ->
+    case Repo.get_by(Entry, title: attrs.title) do
+      nil ->
+        {:ok, _entry} = Entries.create_entry(attrs)
+        {inserted_count + 1, skipped_count}
+
+      %Entry{} ->
+        {inserted_count, skipped_count + 1}
+    end
+  end)
+
+IO.puts("Seeded entries: inserted=#{inserted} skipped=#{skipped}")
