244dc607fa7f7c0943a8102d8a54037910a65669
Nathan Whyte <nathanwhyte35@gmail.com>
2026-02-17 09:01:15 -0600

    Add `credo` for static analysis

--- mix.exs
+++ mix.exs
@@ -50,6 +50,7 @@ defmodule Glossary.MixProject do
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
       {:tailwind_formatter, "~> 0.4.2", only: [:dev, :test], runtime: false},
       {:heroicons,
